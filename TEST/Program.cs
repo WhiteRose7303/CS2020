c937e9c30780e469078c2bc15e831f925eeec552
hadar ovadia <47496403+WhiteRose7303@users.noreply.github.com>
2020-09-10 10:42:49 +0300

    test

--- TEST/Program.cs
+++ TEST/Program.cs
@@ -6,19 +6,8 @@ namespace TEST
     {
         static void Main(string[] args)
         {
-            TET:
             Console.WriteLine("test is complete");
-            int count = 0, tens;
-            for (int i = 0; i<=199; i++)
-            {
-                tens = (i / 10) % 10;
-                if ((tens == 3) || (tens == 7))
-                {
-                    count++;
-                }
-            }
-            Console.WriteLine(count);
-            goto TET;
+            
         }
     }
 }
