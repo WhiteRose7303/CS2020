23427a878882c510fd234204f1b4aad320471ec5
hadar ovadia <47496403+WhiteRose7303@users.noreply.github.com>
2020-09-10 11:52:36 +0300

    10/9

---
+++ Ex-11.11220-Mall-and-Shops/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Ex11.MallandShops
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+    class mall
+    {
+        public static shop[] shops = new shop[200];
+
+        public static bool CheckName(string name, shop[] shops)
+        {
+            for (int i = 0; i < shops.Length; i++)
+            {
+                if (shops[i].GetName()==name)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool CheckMost(shop[] shops)
+        {
+            int count = 0;
+            for (int i = 0; i < shops.Length; i++)
+            {
+                if(shops[i].GetPayed() == true)
+                {
+                    count++;
+                }
+                if (count >= 100)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+    }
+    class shop
+    {
+        public static string name;
+        public static int space;
+        public static int rent;
+        public static bool payed;
+
+        public static int GetRentForM()
+        {
+            int total = 0;
+            total = rent / space;
+            return total;
+        }
+    }
+}
---
+++ Ex-11.11255-California-Fires/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Ex11.CaliforniaFires
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            district[] districts = new district[58];
+        }
+        class fire
+        {
+            public static int windspeed;
+            public static int humidity;
+            public static int tempreture;
+
+            public static void addrisk()
+            {
+                if (windspeed > 70)
+                {
+                    district.SetDanger(district.GetDanger + 1);
+                    if (district.GetDanger() == 5)
+                    {
+                        district.SetDanger(5);
+                    }
+                }
+                if (humidity < 30)
+                {
+                    district.SetDanger(district.GetDanger + 2);
+                    if (district.GetDanger() == 5)
+                    {
+                        district.SetDanger(5);
+                    }
+                }
+                if (tempreture > 100)
+                {
+                    int temp = 100 - tempreture;
+                    int to = temp / 10;
+                    district.SetDanger(district.GetDanger + to);
+                    if (district.GetDanger() == 5)
+                    {
+                        district.SetDanger(5);
+                    }
+                }
+            }
+
+        }
+        class district
+        {
+            public static string name;
+            public static int danger;
+        }
+    }
+}
