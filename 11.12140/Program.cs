a304a5be1004634cc18f6f10d7110e9c905a5b30
hadar ovadia <47496403+WhiteRose7303@users.noreply.github.com>
2020-10-01 09:40:52 +0300

    october

---
+++ 11.12140/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Application
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            printup(8);  
+        }
+        static void printup(int n)
+        {
+            if (n > 1)
+            {
+                printup(n - 1);
+            }
+            Console.WriteLine(n);
+        }
+        static void printdown(int n)
+        {
+            
+        }
+    }
+}
---
+++ stack_test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Unit4.CollectionsLib;
+namespace stack_test
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Stack<int> s = new Stack<int>();
+            Console.WriteLine(s.IsEmpty());
+        }
+    }
+}
