cc5404ebd41069d903d7db7b4ed26cc74f36223c
hadar ovadia <hadarov@tichonet.com>
2020-10-12 14:41:20 +0300

    234

--- Jumping dog/Program.cs
+++ Jumping dog/Program.cs
@@ -1,15 +1,18 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Jumping_dog
 {
     class Program
     {
         static void Main(string[] args)
-        {
+        {            
+            relo();
             Console.Write("steps: ");
             int length = int.Parse(Console.ReadLine());
             for (int i = 0; i < length+1; i++)
                 Console.Write("{0} ", dog(i));
+            
         }
         static int dog(int n)
         {
@@ -19,6 +22,29 @@ namespace Jumping_dog
             else
                 return (dog(n - 1) + dog(n - 2) + dog(n - 3));
         }
-       
+
+
+        public static int[] Fibonacci(int number)
+        {
+            int[] a = new int[number];
+            a[0] = 1;
+            a[1] = 1;
+            a[2] = 2;
+            for (int i = 0; i < number; i++)
+            {
+                a[i] = a[i-3] + a[i - 2] + a[i - 1];
+            }
+            return a;
+        }
+        public static void relo()
+        {
+            int length = int.Parse(Console.ReadLine());
+            var b = Fibonacci(length);
+            foreach (var elements in b)
+            {
+                Console.WriteLine(elements);
+            }
+        }
+
     }
 }
