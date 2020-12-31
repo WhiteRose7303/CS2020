13eee7d6dd1a54d3b8b80c3ee169b85bcc109f31
hadar ovadia <47496403+WhiteRose7303@users.noreply.github.com>
2020-12-31 18:53:36 +0200

    31.12

---
+++ 11-14801/NodeInt/Nodeint.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace NodeInt
+{
+    public class Nodeint
+    {
+        private int value;
+        private Nodeint next;
+        public Nodeint(int v)
+        {
+            this.value = v;
+            this.next = null;
+        }
+        public Nodeint(int v, Nodeint next)
+        {
+            this.value = v;
+            this.next = next;
+        }
+        public override string ToString()
+        {
+            return (this.value.ToString());
+        }
+        public void SetValue(int v)
+        {
+            this.value = v;
+        }
+        public int GetValue()
+        {
+            return this.value;
+        }
+        public void SetNext(Nodeint next)
+        {
+            this.next = next;
+        }
+        public Nodeint GetNext()
+        {
+            return this.next;
+        }
+        public bool HasNext()
+        {
+            return this.next != null;  
+        }
+
+
+
+    }
+}
---
+++ 11-14801/NodeInt/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace NodeInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nodeint n1 = new Nodeint(15);
+            Nodeint n2 = new Nodeint(20,n1);
+            Nodeint n3 = new Nodeint(-1,n2);
+            Nodeint n4 = new Nodeint(5,n3);
+            Console.WriteLine(Print(n4));
+            Console.WriteLine(Print(RetNoNeg(n4)));
+            Console.WriteLine(alabama(n4));
+            
+        }
+        public static string Print(Nodeint n)
+        {
+            Nodeint n1;
+            n1 = n;
+            string str = "";
+            str = str + n1.ToString() + " ";
+            while (n1.HasNext())
+            {
+                n1 = n1.GetNext();
+                str = str + n1.ToString() + " ";
+            }
+            return str;
+
+        }
+        public static Nodeint RetNoNeg(Nodeint n)
+        {
+            Nodeint nt;
+            Nodeint nextnt;
+            nt = n;
+            while (nt.HasNext())
+            {
+                nextnt = nt.GetNext();
+                if (nextnt.GetValue() < 0)
+                    nt.SetNext(nextnt.GetNext());
+                nt = nt.GetNext();
+            }
+            return n;
+        }
+        public static int alabama(Nodeint n)
+        {
+            Nodeint nt;
+            nt = n;
+            int sum = 0;
+            while (nt.HasNext())
+            {
+                sum += nt.GetValue();
+                nt = nt.GetNext();
+            }
+            sum += nt.GetValue();
+            return sum;
+        }
+
+    }
+}
